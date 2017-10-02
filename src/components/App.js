297b6c7cab7b4719e3aff492302f502c842a599b
franco83-2 <franco.84@gmail.com>
2017-10-02 16:37:17 -0700

    added logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import ServiceBoxes from './ServiceBoxes'
+import pubstomp from '../img/pubstomp.jpg'
+import {Grid, Row, Col} from 'react-bootstrap'
 
 
 class App extends Component {
   render() {
 
     return (
-      <ServiceBoxes />
+      <div>
+        <Grid>
+          <Row>
+            <Col xs={6} xsOffset={3}>
+              <img className="img-responsive" src={pubstomp} />
+            </Col>
+          </Row>
+        </Grid>
+        <ServiceBoxes />
+      </div>
     );
   }
 }
